57d6781752fe6e996a4303d6b83f6095a0790b5a
MaxMishchenko <dev.max.frontend.js@gmail.com>
2024-12-31 10:31:10 +0200

    Initial commit

---
+++ gulp/config/tasks.js
@@ -0,0 +1,5 @@
+module.exports = [
+	'./gulp/tasks/html',
+	'./gulp/tasks/scss',
+	'./gulp/tasks/watch'
+];
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+global.$ = {
+    gulp: require('gulp'),
+    del: require('del'),
+    gp: require('gulp-load-plugins')(),
+    strip: require('gulp-strip-comments'),
+
+    path: {
+        tasks: require('./gulp/config/tasks.js')
+    }
+}
+
+$.path.tasks.forEach(function (taskPath) {
+    require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+    $.gulp.parallel('html', 'scss'),
+    $.gulp.parallel('watch')
+));
