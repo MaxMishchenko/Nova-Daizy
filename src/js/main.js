8d558279bfd84cd188f50721e60c63d79d2622f0
MaxMishchenko <dev.max.frontend.js@gmail.com>
2025-01-09 18:22:05 +0200

    New Section

--- src/js/main.js
+++ src/js/main.js
@@ -38,3 +38,37 @@ const elements = document.querySelectorAll('.section__best-price-old');
 elements.forEach(element => {
     observer.observe(element);
 });
+
+//Check
+document.addEventListener('DOMContentLoaded', () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const elements = document.querySelectorAll('.section__reasons-icon');
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+
+                observer.unobserve(entry.target);
+                addVisibleClasses(elements);
+            }
+        });
+    }, observerOptions);
+
+    if (elements.length > 0) {
+        observer.observe(elements[0]);
+    }
+
+    function addVisibleClasses(elements) {
+        elements.forEach((el, index) => {
+            setTimeout(() => {
+                el.classList.add('visible');
+            }, index * 600);
+        });
+    }
+});
+
+
